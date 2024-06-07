c031d2bbbfcff85317a111b31fa386e6d4af216f
Şener Dağ <79213168+senerdag@users.noreply.github.com>
2024-06-07 13:20:01 +0300

    Created Entites and Base Entity

---
+++ MushPredict.Entity/Entities/Kind.cs
@@ -0,0 +1,19 @@
+﻿using MushPredict.Core.Entities;
+using MushPredict.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushPredict.Entity.Entities
+{
+    public class Kind : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public ICollection<Species> Species { get; set; }
+    }
+}
+
---
+++ MushPredict.Entity/Entities/Species.cs
@@ -0,0 +1,19 @@
+﻿using MushPredict.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushPredict.Entity.Entities
+{
+    public class Species : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid KindId { get; set; }
+        public Kind Kind { get; set; }
+        public Guid ImageId { get; set; }
+        public Image Image { get; set; }
+    }
+}
